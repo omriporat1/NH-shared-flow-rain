4ab8ab9f8b60c1248ebe004e5d05e6ff1e2e1254
Omri Porat <152407960+omriporat1@users.noreply.github.com>
2025-02-06 13:54:09 +0200

    Update Best_HPC_omri_trial_cluster.py

--- nhWrap/trial/Best_HPC_omri_trial_cluster.py
+++ nhWrap/trial/Best_HPC_omri_trial_cluster.py
@@ -12,27 +12,8 @@ from neuralhydrology.utils.config import Config
 
 def main():
     if torch.cuda.is_available():
-        start_run(config_file=Path(r"\sci\labs\efratmorin\omripo\PhD\P\NH-shared-flow-rain\nhWrap\trial\config_omri_gpu.yml"))
+        start_run(config_file=Path(r"\sci\labs\efratmorin\omripo\PhD\NH-shared-flow-rain\nhWrap\trial\config_omri_gpu.yml"))
 
-'''
-    run_folder = Path("../neuralhydrology/LSTM_shared_RainDis2Dis/Best_HPC_ensemble_run")
-    num_runs_ensemble = 5
-    # iterate runs for each config file in the folder:
-    for config_file in run_folder.glob("*.yml"):
-        for num_runs_ensemble in range(num_runs_ensemble):
-            run_config = Config(config_file)
-
-            print('model:\t\t', run_config.model)
-            print('seq_length:\t', run_config.seq_length)
-            print('dynamic_inputs:', run_config.dynamic_inputs)
-            if torch.cuda.is_available():
-                start_run(config_file=config_file)
-
-                # fall back to CPU-only mode
-            else:
-                start_run(config_file=config_file,
-                          gpu=-1)
-'''
 
 if __name__ == '__main__':
     main()
