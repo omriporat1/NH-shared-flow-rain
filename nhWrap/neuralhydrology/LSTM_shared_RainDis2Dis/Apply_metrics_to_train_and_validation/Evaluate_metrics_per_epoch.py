bcac86836dae2e55861c5681137a95f021bd5f11
Omri Porat <152407960+omriporat1@users.noreply.github.com>
2025-02-12 11:02:08 +0200

    commit after successful run of metrics calculation

--- nhWrap/neuralhydrology/LSTM_shared_RainDis2Dis/Apply_metrics_to_train_and_validation/Evaluate_metrics_per_epoch.py
+++ nhWrap/neuralhydrology/LSTM_shared_RainDis2Dis/Apply_metrics_to_train_and_validation/Evaluate_metrics_per_epoch.py
@@ -27,9 +27,9 @@ def main():
     num_epochs = run_config.epochs
     # initialize the metrics storage:
     metric_per_epoch_train = pd.DataFrame(
-        columns=["NSE", "MSE", "KGE", "Alpha-NSE", "Beta-NSE", "Pearson-R", "FHV", "FMS", "FLV"])
+        columns=["NSE", "MSE", "KGE", "Alpha-NSE", "Beta-NSE", "Pearson-r", "FHV", "FMS", "FLV"])
     metric_per_epoch_val = pd.DataFrame(
-        columns=["NSE", "MSE", "KGE", "Alpha-NSE", "Beta-NSE", "Pearson-R", "FHV", "FMS", "FLV"])
+        columns=["NSE", "MSE", "KGE", "Alpha-NSE", "Beta-NSE", "Pearson-r", "FHV", "FMS", "FLV"])
 
     # iterate runs for each config file in the folder:
     for epoch in range(num_epochs):
@@ -50,34 +50,46 @@ def main():
         qsim_10_min_val = []
         '''
 
-        all_results = [train_results[basin]["10min"]["xr"]["Flow_m3_sec_zscore_norm_sim"] for basin in train_results.keys()]
-        qsim_10_min_train = xr.concat(all_results, dim='date')
+        all_results_train_sim = [train_results[basin]["10min"]["xr"]["Flow_m3_sec_zscore_norm_sim"] for basin in train_results.keys()]
+        qsim_10_min_train = xr.concat(all_results_train_sim, dim='date')
+        all_results_train_obs = [train_results[basin]["10min"]["xr"]["Flow_m3_sec_zscore_norm_obs"] for basin in train_results.keys()]
+        qobs_10_min_train = xr.concat(all_results_train_obs, dim='date')
 
+        all_results_val_sim = [val_results[basin]["10min"]["xr"]["Flow_m3_sec_zscore_norm_sim"] for basin in val_results.keys()]
+        qsim_10_min_val = xr.concat(all_results_val_sim, dim='date')
+        all_results_val_obs = [val_results[basin]["10min"]["xr"]["Flow_m3_sec_zscore_norm_obs"] for basin in val_results.keys()]
+        qobs_10_min_val = xr.concat(all_results_val_obs, dim='date')
+
+        '''
         for basin in train_results.keys():
             qsim_10_min_train = qsim_10_min_train.append(train_results[basin]["10min"]["xr"]["Flow_m3_sec_zscore_norm_sim"])
             qobs_10_min_train = qobs_10_min_train.append(train_results[basin]["10min"]["xr"]["Flow_m3_sec_zscore_norm_obs"])
+        '''
         values_train = metrics.calculate_all_metrics(qobs_10_min_train.isel(time_step=-1), qsim_10_min_train.isel(time_step=-1))
 
+        '''
         for basin in val_results.keys():
             qsim_10_min_val = qsim_10_min_val.append(val_results[basin]["10min"]["xr"]["Flow_m3_sec_zscore_norm_sim"])
             qobs_10_min_val = qobs_10_min_val.append(val_results[basin]["10min"]["xr"]["Flow_m3_sec_zscore_norm_obs"])
+        '''
         values_val = metrics.calculate_all_metrics(qobs_10_min_val.isel(time_step=-1), qsim_10_min_val.isel(time_step=-1))
 
         # calculate some metrics for the training and validation period of the current epoch:
+        '''
         print(f"Epoch: {epoch}")
         print("10-min metrics:")
         for key, val in values_train.items():
             print(f"train  {key}: {val:.3f}")
         for key, val in values_val.items():
             print(f"val  {key}: {val:.3f}")
-
+        '''
         # save metrics in the dataframe:
         metric_per_epoch_train.loc[epoch, "NSE"] = values_train["NSE"]
         metric_per_epoch_train.loc[epoch, "MSE"] = values_train["MSE"]
         metric_per_epoch_train.loc[epoch, "KGE"] = values_train["KGE"]
         metric_per_epoch_train.loc[epoch, "Alpha-NSE"] = values_train["Alpha-NSE"]
         metric_per_epoch_train.loc[epoch, "Beta-NSE"] = values_train["Beta-NSE"]
-        metric_per_epoch_train.loc[epoch, "Pearson-R"] = values_train["Pearson-R"]
+        metric_per_epoch_train.loc[epoch, "Pearson-r"] = values_train["Pearson-r"]
         metric_per_epoch_train.loc[epoch, "FHV"] = values_train["FHV"]
         metric_per_epoch_train.loc[epoch, "FMS"] = values_train["FMS"]
         metric_per_epoch_train.loc[epoch, "FLV"] = values_train["FLV"]
@@ -87,7 +99,7 @@ def main():
         metric_per_epoch_val.loc[epoch, "KGE"] = values_val["KGE"]
         metric_per_epoch_val.loc[epoch, "Alpha-NSE"] = values_val["Alpha-NSE"]
         metric_per_epoch_val.loc[epoch, "Beta-NSE"] = values_val["Beta-NSE"]
-        metric_per_epoch_val.loc[epoch, "Pearson-R"] = values_val["Pearson-R"]
+        metric_per_epoch_val.loc[epoch, "Pearson-r"] = values_val["Pearson-r"]
         metric_per_epoch_val.loc[epoch, "FHV"] = values_val["FHV"]
         metric_per_epoch_val.loc[epoch, "FMS"] = values_val["FMS"]
         metric_per_epoch_val.loc[epoch, "FLV"] = values_val["FLV"]
@@ -107,8 +119,6 @@ def main():
     # save the metrics to a csv file:
     metric_per_epoch_train.to_csv(run_dir / "train_metrics.csv")
     metric_per_epoch_val.to_csv(run_dir / "val_metrics.csv")
-
-
 
 
 if __name__ == '__main__':
